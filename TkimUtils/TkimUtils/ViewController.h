eed6a50eaf21b718d9a048b9762afc1b6eeadf22
taikangMOA <pengyuan04>
2018-07-10 14:13:10 +0800

    create

---
+++ TkimUtils/TkimUtils/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TkimUtils
+//
+//  Created by pengyuan on 2018/7/10.
+//  Copyright © 2018年 pengyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TkimUtils/TkimUtils/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TkimUtils
+//
+//  Created by pengyuan on 2018/7/10.
+//  Copyright © 2018年 pengyuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
